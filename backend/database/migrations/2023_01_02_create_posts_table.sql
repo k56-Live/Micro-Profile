3ffe57336c9585d96201b93b92ec9cbed76de508
k56 Live <139924674+k56-Live@users.noreply.github.com>
2023-07-27 23:41:31 +0530

    Create 2023_01_02_create_posts_table.sql

---
+++ backend/database/migrations/2023_01_02_create_posts_table.sql
@@ -0,0 +1,8 @@
+CREATE TABLE IF NOT EXISTS posts (
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    content TEXT NOT NULL,
+    user_id INTEGER REFERENCES users(id),
+    created_at TIMESTAMP NOT NULL DEFAULT NOW(),
+    updated_at TIMESTAMP NOT NULL DEFAULT NOW()
+);
