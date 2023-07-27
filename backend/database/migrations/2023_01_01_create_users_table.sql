ff909dbe71762c5dc3a15aa91d77338a8fb98c49
k56 Live <139924674+k56-Live@users.noreply.github.com>
2023-07-27 23:40:51 +0530

    Create 2023_01_01_create_users_table.sql

---
+++ backend/database/migrations/2023_01_01_create_users_table.sql
@@ -0,0 +1,8 @@
+CREATE TABLE IF NOT EXISTS users (
+    id SERIAL PRIMARY KEY,
+    username VARCHAR(50) NOT NULL,
+    email VARCHAR(100) NOT NULL,
+    password VARCHAR(100) NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT NOW(),
+    updated_at TIMESTAMP NOT NULL DEFAULT NOW()
+);
